7b43284bdc204635609f482786d39fbadc245ccb
ashbyjibin <83183554+ashbyjibin@users.noreply.github.com>
2022-01-29 17:46:30 +0530

    Update models.py

--- Razorpay_project/razorpayapp/models.py
+++ Razorpay_project/razorpayapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class razorpay_table(models.Model):
+    razorpay_order_id = models.CharField(max_length=200)
+    razorpay_payment_id = models.CharField(max_length=200, default='')
+    razorpay_signature = models.CharField(max_length=200, default='')
+ 
+   
+
